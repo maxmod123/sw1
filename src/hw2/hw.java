dc0e90f7804bb985dfab414490e69e7786ceab50
kareemabusoud <kareem_maxlove@hotmail.com>
2019-02-18 18:27:22 +0200

    done

--- src/hw2/hw.java
+++ src/hw2/hw.java
@@ -1,9 +1,53 @@
 package hw2;
 
+import java.util.ArrayList;
+
 public class hw {
+	public static class card {
+		String name ; 
+		int balance ; 
+		 int amount ;
+		ArrayList <String>  books  = new ArrayList <String>() ;
+		card (String name  ){
+		
+			 this.balance = 0 ; 
+			 this.name=name ;
+			 this.amount = 0;
+			
+			
+			
+		}
+		card (String name , int balance){
+			this.name=name  ;
+			this.balance=balance ;
+		}
+		 void addbooks (books x1) {
+			 books.add(x1.name);
+			 amount+=x1.price;
+		 }
+		 	void updatebalance ( String name  ,int balance1) {
+			this.balance= balance1 ; 
+		}
+	}
+	public  static class books{
+		books(String name , int price ){
+			this.name = name ; 
+			this.price= price ; 
+		}
+		String name ; 
+		int  price ; 
+		
+		
+	} 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		card x = new card("kareem", 400);
+	books java=	 new books("java",170);
+	books web = new books("web",100);
+	x.addbooks(java);
+	x.addbooks(web);
+	
+		
 
 	}
 
